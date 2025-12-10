0997e27bfefed3a70e7dccf64785861373bb5252
firdaush15 <mohd52806@gmail.com>
2025-12-10 16:12:40 +0800

    Add files via upload

---
+++ Models/PrescriptionItem.php
@@ -0,0 +1,26 @@
+<?php
+
+// PrescriptionItem.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PrescriptionItem extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'item_id';
+
+    protected $fillable = [
+        'prescription_id',
+        'medicine_name',
+        'dosage',
+        'frequency',
+    ];
+
+    public function prescription()
+    {
+        return $this->belongsTo(Prescription::class, 'prescription_id');
+    }
+}
